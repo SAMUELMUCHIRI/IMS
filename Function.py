aca4867defc481c0f4517ac1a500796832013634
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-06-18 20:52:46 +0300

    Creating more functions

--- Function.py
+++ Function.py
@@ -17,6 +17,21 @@ def modify_path():
     actualpath=checkpath()
     new_modifiedpath=actualpath+newpath
     return new_modifiedpath
+def saveentry():
+    getpath=(modify_path()+"\\interns.csv")
+    entry=open(getpath,'a',newline=' ')
+    csvframe=csv.writer(entry)
+    #all these are to become new entries in forms to be considered during ui design
+    Interns_name=str(input('Interns Name'))
+    Campus=str(input('Campus'))
+    Phone_number=str(input('Phone Number'))
+    From_Start=str(input("Start"))
+    To_end=str(input("To "))
+    Supervisor=str(input('Supervisor'))
+    listclass=[Interns_name,Campus,Phone_number,From_Start,To_end,Supervisor]
+    csvframe.writerow(listclass)
+    #The print functon is to become a pop up later
+    print('Entry is recorded')
 
     
     
@@ -36,5 +51,4 @@ def savepassword():
 def login_page(email):
     print("   ")
 
-checkpath()
-modify_path()
+saveentry()
