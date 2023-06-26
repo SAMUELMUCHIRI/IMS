b89b7f87ae375a95b383d64219450149bd5b43b8
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-06-26 15:19:15 +0300

    figuringout how to install java libraries lol

--- variablej.java
+++ variablej.java
@@ -1,21 +1,29 @@
 public class variablej {
+    /* naming a new type */
+    enum DAY {SUNDAY , MONDAY , TUSDAY, WEDNESDAY, THURSDAY,FRIDAY,SATURDAY}
     public static void main(String[] args){
+        /* declaring variables  */
         double princ_iple;
         double finale;
         double rate;
         double time;
         String cate;
+        DAY Day;
+        String joke;
 
         cate="seize the day ";
+        Day=DAY.SUNDAY ;
 
         princ_iple =40000;
         rate = 0.3 ;
-
-        time=7;
+        time=3;
+        joke=(System.in("time"));
+        
         finale= princ_iple * rate * time;
         System.out.println(Math.sqrt(finale));
         System.out.println(finale);
-        System.out.println(cate.toUpperCase());
+        System.out.printf( "%1.2f",finale);
+        System.out.println(cate.toUpperCase()+" "+ Day);
 
 
 
