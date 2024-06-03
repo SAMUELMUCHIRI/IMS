edd950720db2b3ac41a76a75b4bd5bebfaf1e525
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2024-06-03 23:43:04 +0300

    001

---
+++ hello.cs
@@ -0,0 +1,13 @@
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r;
+            r = 20;
+            Console.WriteLine("Hello World!  " + r);
+        }
+    }
+}
